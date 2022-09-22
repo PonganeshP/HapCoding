88527614683f3253f2545c44a92a7e4e4ec0890b
kal-el <74044780+PonganeshP@users.noreply.github.com>
2022-09-22 13:07:38 +0530

    Arithmetic modulo assignment (mostly long should be used to prevent overflow)

---
+++ src/Day15/PowerWithModulo.java
@@ -0,0 +1,11 @@
+package Day15;
+
+public class PowerWithModulo {
+    public int solve(int A, int B, int C) {
+        long ans=1;
+        for(int i=1;i<=B;i++){
+            ans=(ans*(A%C))%C;
+        }
+        return (int)ans;
+    }
+}
