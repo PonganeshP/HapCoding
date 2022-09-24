376e49653a0cd2d4b875fd825d6e24829654322a
kal-el <74044780+PonganeshP@users.noreply.github.com>
2022-09-24 10:50:21 +0530

    sorting in java

---
+++ src/Day17/SortByColour.java
@@ -0,0 +1,11 @@
+package Day17;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortByColour {
+    public ArrayList<Integer> sortColors(ArrayList<Integer> A) {
+        Collections.sort(A);
+        return A;
+    }
+}
