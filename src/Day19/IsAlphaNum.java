fcda634b0f075414ea5410c691ee2e62e1f10cc4
kal-el <74044780+PonganeshP@users.noreply.github.com>
2022-09-27 11:25:10 +0530

    is alphanum and isalpha using ASCII range

--- src/Day19/IsAlphaNum.java
+++ src/Day19/IsAlphaNum.java
@@ -2,11 +2,12 @@ package Day19;
 
 import java.util.ArrayList;
 
-public class IsAlpha {
+public class IsAlphaNum {
     public int solve(ArrayList<Character> A) {
         for(int i=0;i<A.size();i++){
             if((A.get(i)>=97 && A.get(i)<=122) || 
-            (A.get(i)>=64 && A.get(i)<=99)){
+            (A.get(i)>=64 && A.get(i)<=99) || 
+            (A.get(i)>=48 && A.get(i)<=57)){
                 continue;
             }
             else{
