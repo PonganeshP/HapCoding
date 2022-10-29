d4444d424e6d20b052ca988ff31fc6b9dea3c84c
kal-el <74044780+PonganeshP@users.noreply.github.com>
2022-10-29 14:37:41 +0530

    Two sum using hashing

---
+++ src/LeetCode/L1.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> hm=new HashMap<>();
+        int[] res=new int[2];
+        for(int i=0;i<nums.length;i++){
+            if(!hm.containsKey(target-nums[i])){
+                hm.put(nums[i],i);
+            }
+            else{
+                res[0]=hm.get(target-nums[i]);
+                res[1]=i;
+                break;
+            }
+        }
+        return res;
+    }
+}
