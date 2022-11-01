fe9c32d1e73e86c8d0bea496198ee80ccd6df98b
kal-el <74044780+PonganeshP@users.noreply.github.com>
2022-11-01 20:07:09 +0530

    leet code other opt soln

--- src/LeetCode/L1131.java
+++ src/LeetCode/L1131.java
@@ -4,37 +4,51 @@ package LeetCode;
 
 public class L1131 {
     public int maxAbsValExpr(int[] arr1, int[] arr2) {
-        int[] X=new int[arr1.length];
-        int[] Y=new int[arr1.length];
-        int[] Z1=new int[arr1.length];
-        int[] Z2=new int[arr1.length];
+        int finalRes=Integer.MIN_VALUE;
         int xMax=Integer.MIN_VALUE;
         int xMin=Integer.MAX_VALUE;
-        int yMax=Integer.MIN_VALUE;
-        int yMin=Integer.MAX_VALUE;
-        int z1Max=Integer.MIN_VALUE;
-        int z1Min=Integer.MAX_VALUE;
-        int z2Max=Integer.MIN_VALUE;
-        int z2Min=Integer.MAX_VALUE;
-        for(int i=0;i<arr1.length;i++){
-            X[i]=arr1[i]+arr2[i]+i;
-            Y[i]=arr1[i]+arr2[i]-i;
-            xMax=Math.max(xMax,X[i]);
-            xMin=Math.min(xMin,X[i]);
-            yMax=Math.max(yMax,Y[i]);
-            yMin=Math.min(yMin,Y[i]);
-            
-            Z1[i]=arr1[i]-arr2[i]-i;
-            Z2[i]=arr1[i]-arr2[i]+i;
-            z1Max=Math.max(z1Max,Z1[i]);
-            z1Min=Math.min(z1Min,Z1[i]);
-            z2Max=Math.max(z2Max,Z2[i]);
-            z2Min=Math.min(z2Min,Z2[i]);
-        }
-        int res1=Math.max(xMax-xMin,yMax-yMin);
-        int res2=Math.max(z1Max-z1Min,z2Max-z2Min);
-        int finalRes=Math.max(res1,res2);
-        return finalRes;
         
+        for(int i=0;i<8;i++){
+            for(int j=0;j<arr1.length;j++){
+                int currSum=arr1[j];
+                for(int k=0;k<2;k++){
+                    int temp=0;
+                    if(k==0)
+                        temp=arr2[j];
+                    else if(k==1)
+                        temp=j;
+                    if((i & (1<<k))!=0)
+                        currSum+=temp;
+                    else
+                        currSum-=temp;
+                }
+                xMax=Math.max(xMax,currSum);
+                xMin=Math.min(xMin,currSum);
+            }
+            finalRes=Math.max(xMax-xMin,finalRes);
+        }
+        // int xMax=Integer.MIN_VALUE;
+        // int xMin=Integer.MAX_VALUE;
+        // int yMax=Integer.MIN_VALUE;
+        // int yMin=Integer.MAX_VALUE;
+        // int z1Max=Integer.MIN_VALUE;
+        // int z1Min=Integer.MAX_VALUE;
+        // int z2Max=Integer.MIN_VALUE;
+        // int z2Min=Integer.MAX_VALUE;
+        // for(int i=0;i<arr1.length;i++){
+        //     xMax=Math.max(xMax,arr1[i]+arr2[i]+i);
+        //     xMin=Math.min(xMin,arr1[i]+arr2[i]+i);
+        //     yMax=Math.max(yMax,arr1[i]+arr2[i]-i);
+        //     yMin=Math.min(yMin,arr1[i]+arr2[i]-i);
+            
+        //     z1Max=Math.max(z1Max,arr1[i]-arr2[i]-i);
+        //     z1Min=Math.min(z1Min,arr1[i]-arr2[i]-i);
+        //     z2Max=Math.max(z2Max,arr1[i]-arr2[i]+i);
+        //     z2Min=Math.min(z2Min,arr1[i]-arr2[i]+i);
+        // }
+        // int res1=Math.max(xMax-xMin,yMax-yMin);
+        // int res2=Math.max(z1Max-z1Min,z2Max-z2Min);
+        // int finalRes=Math.max(res1,res2);
+        return finalRes;
     }
 }
