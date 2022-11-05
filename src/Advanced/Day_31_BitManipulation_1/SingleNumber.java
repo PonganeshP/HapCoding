b7207284f3c12f59feb745aebff7b7600b3474c1
kal-el <74044780+PonganeshP@users.noreply.github.com>
2022-11-05 16:17:04 +0530

    single number prblm statement

--- src/Advanced/Day_31_BitManipulation_1/SingleNumber.java
+++ src/Advanced/Day_31_BitManipulation_1/SingleNumber.java
@@ -1,5 +1,31 @@
 package Advanced.Day_31_BitManipulation_1;
 
+/*
+ * Problem Description
+Given an array of integers, every element appears thrice except for one, which occurs once.
+
+Find that element that does not appear thrice.
+
+NOTE: Your algorithm should have a linear runtime complexity.
+
+Could you implement it without using extra memory?
+
+Example Input
+Input 1:
+
+ A = [1, 2, 4, 3, 3, 2, 2, 3, 1, 1]
+
+ Example Output
+Output 1:
+
+ 4
+
+ Example Explanation
+Explanation 1:
+
+ 4 occurs exactly once in Input 1.
+ */
+
 public class SingleNumber {
     public int singleNumber(final int[] A) {
         int ans=0;
