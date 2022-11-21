e2d159ad1da8a3a4df38858eb1081c05f80bb9d8
kal-el <74044780+PonganeshP@users.noreply.github.com>
2022-11-21 10:34:46 +0530

    luck num prblm statement

--- src/Advanced/Day_35_PrimeNumber/LuckyNum.java
+++ src/Advanced/Day_35_PrimeNumber/LuckyNum.java
@@ -1,4 +1,36 @@
 package Advanced.Day_35_PrimeNumber;
+/*
+ * Problem Description
+A lucky number is a number that has exactly 2 distinct prime divisors.
+
+You are given a number A, and you need to determine the count of lucky numbers between the range 1 to A (both inclusive).
+
+Example Input
+Input 1:
+
+ A = 8
+Input 2:
+
+ A = 12
+
+ Example Output
+Output 1:
+
+ 1
+Output 2:
+
+ 3
+
+
+Example Explanation
+Explanation 1:
+
+ Between [1, 8] there is only 1 lucky number i.e 6.
+ 6 has 2 distinct prime factors i.e 2 and 3.
+Explanation 2:
+
+ Between [1, 12] there are 3 lucky number: 6, 10 and 12.
+ */
 
 public class LuckyNum {
     public int solve(int A) {
